f4c2cbe7266672f5b7c05e23215015b752812c75
superstar <83311833+clever-web@users.noreply.github.com>
2022-11-17 09:08:17 +0200

    Update Document.php

--- app/Document.php
+++ app/Document.php
@@ -1,41 +1,41 @@
-<?php
+    <?php
 
-namespace App;
+    namespace App;
 
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Storage;
+    use Illuminate\Database\Eloquent\Model;
+    use Illuminate\Support\Facades\Storage;
 
-class Document extends Model
-{
-    protected $fillable = [
-        'organization_id',
-        'name', 
-        'description',
-        'filename',
-        'path',
-    ];
-
-    public function organization()
+    class Document extends Model
     {
-        return $this->belongsTo( Organization::class );
+        protected $fillable = [
+            'organization_id',
+            'name', 
+            'description',
+            'filename',
+            'path',
+        ];
+
+        public function organization()
+        {
+            return $this->belongsTo( Organization::class );
+        }
+
+        /**
+         * The "booting" method of the model.
+         *
+         * @return void
+         */
+        protected static function boot()
+        {
+            parent::boot();
+
+            static::deleted( function($document) {
+
+                if( $document->path )
+                {
+                    Storage::delete($document->path);
+                }
+                
+            });
+        }        
     }
-
-    /**
-     * The "booting" method of the model.
-     *
-     * @return void
-     */
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::deleted( function($document) {
-
-            if( $document->path )
-            {
-                Storage::delete($document->path);
-            }
-            
-        });
-    }        
-}
